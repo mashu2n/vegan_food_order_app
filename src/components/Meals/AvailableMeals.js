f87d3b6b39b028346fd12014c33c331c6f456d3a
Gerrin Murray <50505367+mashu2n@users.noreply.github.com>
2022-08-12 12:52:03 -0500

    fixed naming and added outputting for for cart items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,7 +32,8 @@ const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
-      name={meal.id}
+      id={meal.id}
+      name={meal.name}
       description={meal.description}
       price={meal.price}
     />
